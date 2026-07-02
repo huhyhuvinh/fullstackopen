62ad8a9f8698d42366e050360516f4cf0d6f548a
huhyhuvinh <vinhtran048@gmail.com>
2026-07-02 21:28:27 +0700

    feat: completed part 1 exercises

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,53 @@
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>{props.name} {props.exercises}</p>
+    )
+}
+
+const Content = (props) => {
+    return (
+        props.parts.map(part => <Part key={part.name} name={part.name} exercises={part.exercises} />)
+    )
+}
+
+const Total = (props) => {
+    let total = 0
+    props.parts.forEach(part => total = total + part.exercises)
+    return (
+        <p>Number of exercises {total}</p>
+    )
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+export default App
